8b4e26752330a27dba5586f2e527aecac3ce1292
Dominic01prog <djs7129@psu.edu>
2020-09-10 23:00:38 +0000

    changed variable name

--- main.c
+++ main.c
@@ -9,34 +9,34 @@
 int main(void) {
   char *grade = readline("Enter your CMPSC 131 grade: ");
   double percentage = atof(grade);
-  char *letter_grade = 0;
+  char *letter = 0;
   if(percentage >= 93.0) {
-    letter_grade = "A";
+    letter = "A";
   }
   else if(percentage >= 90.0) {
-    letter_grade = "A-";
+    letter = "A-";
   }
   else if(percentage >= 87.0) {
-    letter_grade = "B+";
+    letter = "B+";
   }
   else if(percentage >= 83.0) {
-    letter_grade = "B";
+    letter = "B";
   }
   else if(percentage >= 80.0) {
-    letter_grade = "B-";
+    letter = "B-";
   }
   else if(percentage >= 77.0) {
-    letter_grade = "C+";
+    letter = "C+";
   }
   else if(percentage >= 70.0) {
-    letter_grade = "C";
+    letter = "C";
   }
   else if(percentage >= 60.0) {
-    letter_grade = "D";
+    letter = "D";
   }
   else {
-    letter_grade = "F";
+    letter = "F";
   }
-  printf("Your letter grade for CMPSC 131 is %s.", letter_grade);
+  printf("Your letter grade for CMPSC 131 is %s.\n", letter);
   return 0;
 }
